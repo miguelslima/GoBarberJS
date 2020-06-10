247482887b2aea01e6dbffdb27819597b37f371f
Miguel <miguelsousalima@hotmail.com>
2020-06-10 09:51:05 -0300

    Appointment Controller

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ import UserController from './app/controllers/UserControler';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -23,6 +25,11 @@ routes.put('/users', UserController.update);
 
 // Proveiders
 routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
