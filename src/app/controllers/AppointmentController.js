6bae857fc200a430166990480da39dbfb5ac8de5
Miguel <miguelsousalima@hotmail.com>
2020-06-11 02:06:12 -0300

    Envio de notificaçoes MongoDB

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notification from '../schemas/notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -55,6 +57,12 @@ class AppointmentController {
         .json({ error: 'You can only create appointmets with providers' });
     }
 
+    if (provider_id === req.userId) {
+      return res
+        .status(400)
+        .json({ error: 'You cannot schedule with yourself' });
+    }
+
     const hourStart = startOfHour(parseISO(date));
 
     // Check for past dates
@@ -81,6 +89,19 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    // Notify appointment provider
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointmet);
