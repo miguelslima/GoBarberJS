1263cd6fc3f679534a15bc5184fa25a558281ff6
Miguel <miguelsousalima@hotmail.com>
2020-07-01 09:54:21 -0300

    Notificacao, header, Funcionalidades Perfil

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
@@ -7,7 +8,9 @@ import logo from '~/assets/logoHeader.svg';
 
 import { Container, Content, Profile } from './styles';
 
-export default function index() {
+export default function Header() {
+  const profile = useSelector((state) => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -21,11 +24,14 @@ export default function index() {
 
           <Profile>
             <div>
-              <strong>Miguel Lima</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
               alt="Miguel Lima"
             />
           </Profile>
--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,39 +1,50 @@
 import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { updateProfileRequest } from '../../store/modules/user/actions';
+import { signOut } from '../../store/modules/auth/actions';
+
+import AvatarInput from './AvatarInput';
 
 import { Container } from './styles';
 
-function Profile() {
+export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector((state) => state.user.profile);
 
-  function handleSubmit(data) {}
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
 
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
-        <Input name="name" placeholder="Nome completo" />
+        <AvatarInput name="avatar_id" />
+
+        <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu endereço de email" />
 
         <hr />
+
+        <Input name="oldPassword" type="password" placeholder="Senha atual" />
+        <Input name="password" type="password" placeholder="Nova senha" />
         <Input
-          type="password"
-          name="oldPassword"
-          placeholder="Sua senha atual"
-        />
-        <Input type="password" name="password" placeholder="Nova senha" />
-        <Input
-          type="password"
           name="confirmPassword"
-          placeholder="Confirme a senha"
+          type="password"
+          placeholder="Confirmação de senha"
         />
 
         <button type="submit">Atualizar perfil</button>
       </Form>
 
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
     </Container>
   );
 }
-
-export default Profile;
